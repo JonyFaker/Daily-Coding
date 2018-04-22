f82566ccf759babc470f30c3ea145ec1086fa7bc
feng jiang <352815247@qq.com>
2018-04-22 18:50:34 +0800

    wait for test 002

---
+++ 剑指offer/001.cpp
@@ -0,0 +1,61 @@
+/*
+在一个二维数组中，每一行都按照从左到右递增的顺序排序，
+每一列都按照从上到下递增的顺序排序。请完成一个函数，
+输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+*/
+
+// #include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class Solution {
+public:
+    bool Find(int target, vector<vector<int> > array) {
+        int index_m, index_n = 0;
+        int m = array.size();
+        int n = array[0].size();
+        printf("M: %d, N: %d \n", m, n);
+        //从右上角开始查找
+        index_m = 0;
+        index_n = n-1;
+        int temp = array[index_m][index_n];
+        while(target != temp && index_m <= m-1 && index_n >= 0){
+        	temp = array[index_m][index_n];
+        	printf("location:[%d, %d]: %d\n", index_m, index_n, temp);
+            if(target < temp && index_n > 0)
+            {
+                //向左移动一列
+                index_n --;
+            }else if(target > temp && index_m < m-1)
+            {
+                //向下移动一行
+                index_m ++;
+            }else if(target == temp && index_m <= m-1 && index_n >= 0)
+            {
+                return true;
+            }else
+            {
+            	return false;
+            }
+        }
+        if(temp == target)
+            return true;
+        return false;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+	Solution* solution = new Solution();
+	std::vector<std::vector<int > > array = {{1,2,3,4,5}, {2,3,4,5,6}, {5,6,7,8,9}};
+	if (argc != 2)
+	{
+		printf("set target!\n");
+		return -1;
+	}
+	int target = atoi(argv[1]);
+	std::cout<<"result: "<<solution->Find(target, array)<<std::endl;
+	return 0;
+}
---
+++ 剑指offer/002.cpp
@@ -0,0 +1,86 @@
+/*
+请实现一个函数，将一个字符串中的空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <sys/malloc.h>
+#include <memory.h>
+
+class Solution {
+public:
+	void replaceSpace(char *str,int length) {
+		//当几个空格连续的时候呢？
+		char* dest = new char[length * 3];
+		int count = 0;
+		bool last_is_space = false;
+		int j = 0;
+		int space_count = 0;
+		for (int i = 0; i < length; ++i)
+		{
+			if (str[i] == ' ')
+			{
+				space_count = 1;
+				while(str[i+1] == ' ')
+				{
+					space_count ++;
+					i ++;
+				}
+				dest[j] = '%';
+				j++;
+				dest[j] = '2';
+				j++;
+				dest[j] = '0';
+			}else
+			{
+				dest[j] = str[i];
+			}
+			j++;
+		}
+		dest[j] = '\0';
+		std::cout<<"dest: "<<dest<<std::endl;
+
+		str = (char*)remalloc(j*sizeof(char));
+		strcpy(str, dest);
+		std::cout<<"str: "<<str<<std::endl;
+	}
+};
+
+
+int main(int argc, char const *argv[])
+{
+	Solution* sulotion = new Solution();
+	char str[] = "hello  world ! ";
+	std::cout<<str<<std::endl;
+	printf("str length: %d \n", strlen(str));
+	sulotion->replaceSpace(str, strlen(str));
+	std::cout<<str<<std::endl;
+	return 0;
+}
+
+/*
+////////////////////////////////////////////////////////////////////////////
+在一开始做这条题目的时候，声明str的时候我使用的是char *str="hello  world ! "
+但，这样声明的是一个字符串常亮，无法改变其值，所以需要换成数组的声明方式。
+例子：
+char ss[]="C++";  
+ss[0]='c';                  // 合法  
+char *p="C++";  
+p[0]='c';                   // 合法但不正确  
+
+总结：
+（1）char ss[]定义了一个数组，ss可认为是一个常指针，ss不可改变，但ss指向的内容可以发生改变。
+
+（2）char *p定义了一个可变指针，p可以指向其它对象。但对于char *p=”abc“这样的情况，p指向的是常量，故内容不能改变。
+
+参考博客：https://blog.csdn.net/szchtx/article/details/10396149
+
+////////////////////////////////////////////////////////////////////////////
+这条题目还要求在原来的内存地址上储存改变之后的值，所以我想使用remalloc函数
+////////////////////////////////////////////////////////////////////////////
+*/
+
+
+
